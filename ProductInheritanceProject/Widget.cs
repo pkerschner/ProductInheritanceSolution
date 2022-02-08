be85bda0bc6e85ac75c205976946cf23133539c8
Philip Kerschner <pkerschner@zoominternet.net>
2022-02-08 14:11:06 -0500

    Add project files.

---
+++ ProductInheritanceProject/Widget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInheritanceProject {
+    public class Widget : Product {
+
+        public string Size { get; set; } = "Medium";
+        public string Color { get; set; } = "Black";
+
+        public override string ToPrint() {
+            return $"{base.ToPrint()} | {Size} | {Color}";
+        }
+
+        public Widget(string code, decimal price, string size, string color) 
+            : base(code, price) {
+
+            Size = size;
+            Color = color;
+
+        }
+    }
+}
